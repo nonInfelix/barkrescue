d3414ff9eec4bff05a29927b9c77d1c7e4cc0bf8
Felix Volkmer <noninfelix@gmail.com>
2023-11-02 18:11:10 +0100

    added arguments on startpage

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,4 @@
 <template>
   <HeaderComponent></HeaderComponent>
   <slot />
-  <FooterComponent></FooterComponent>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,33 @@
       <button class="btn">Hund freigeben</button>
     </div>
   </main>
+  <article class="container arguments">
+    <h2 class="argument-question">Wieso BarkRescue?</h2>
+    <section class="s-1">
+      <h3>Rettung eines Lebens</h3>
+      <p>
+        Durch die Adoption aus einem Tierheim erhalten Sie die Möglichkeit,
+        einem Tier ein neues Zuhause zu geben und sein Leben zu retten. Sie
+        schenken einem hilfsbedürftigen Tier eine zweite Chance.
+      </p>
+    </section>
+    <section class="s-2">
+      <h3>Vielfalt an Auswahl</h3>
+      <p>
+        Tierheime beherbergen Tiere unterschiedlicher Rassen, Größen,
+        Altersgruppen und Persönlichkeiten. Sie können das Tier finden, das am
+        besten zu Ihrem Lebensstil und Ihren Bedürfnissen passt.
+      </p>
+    </section>
+    <section class="s-3">
+      <h3>Unterstützung von Tierschutzorganisationen</h3>
+      <p>
+        Indem Sie ein Tier aus dem Tierheim adoptieren, unterstützen Sie die
+        wichtige Arbeit von Tierschutzorganisationen und helfen dabei, die
+        Überbelegung in Tierheimen zu verringern.
+      </p>
+    </section>
+  </article>
 </template>
 
 <script setup lang="ts">
@@ -49,5 +76,46 @@ main.container {
   justify-self: center;
   align-self: end;
   margin-bottom: 10px;
+}
+
+h2.argument-question {
+  margin-bottom: 1rem;
+  padding: 5px;
+  color: white;
+  text-align: center;
+  grid-row: 1/1;
+  grid-column: 1/10;
+}
+h3 {
+  color: $s-green;
+}
+p {
+  color: white;
+}
+section {
+  padding: 0 0.5rem 0 0.5rem;
+  margin-bottom: 1rem;
+  z-index: 1;
+}
+article.arguments {
+  display: grid;
+  grid-template-rows: 10% 1fr 1fr 1fr;
+  grid-template-columns: repeat(10, 1fr);
+  background: rgb(20, 40, 29);
+  padding: 0.5rem;
+  margin: 1rem 1rem;
+  border-radius: 12px;
+}
+.s-1 {
+  grid-row: 2/2;
+  grid-column: 1/-1;
+}
+.s-2 {
+  grid-row: 3/3;
+  grid-column: 1/-1;
+}
+.s-3 {
+  grid-row: 4/4;
+  grid-column: 1/-1;
 }
 </style>
