39ff18758c6a34e5f144128a7b3893821db84016
Felix Volkmer <noninfelix@gmail.com>
2023-11-05 17:29:33 +0100

    added pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
   modules: [
     "nuxt-icon",
     "@nuxtjs/supabase",
+    "@pinia/nuxt",
     [
       "@nuxtjs/google-fonts",
       {
--- pages/index.vue
+++ pages/index.vue
@@ -105,6 +105,8 @@ article.arguments {
   padding: 0.5rem;
   margin: 1rem 1rem;
   border-radius: 12px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
 }
 .s-1 {
   grid-row: 2/2;
