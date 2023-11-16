488f1aa05645c590f4b4087de9b78af48295912d
Felix Volkmer <noninfelix@gmail.com>
2023-11-16 14:34:53 +0100

    added general styling for LongOfferComponent

--- components/LongOfferComponent.vue
+++ components/LongOfferComponent.vue
@@ -15,28 +15,69 @@
       <button class="dog-button">Jetzt anfragen</button>
     </main>
     <section class="breed-container">
-      <h2>Rasseportrait</h2>
-      <img :src="breed.img" alt="" />
-      <article>
-        <h3>körperliche Merkmale</h3>
-        <p>Rasse: {{ breed.name }}</p>
-        <p>Größenzuordnung: {{ breed.category }}</p>
-        <p>Gewicht: {{ breed.weight }}</p>
-        <p>Größe: {{ breed.size }}</p>
-        <p>Farben: {{ breed.color }}</p>
-        <p>Lebenserwartung: {{ breed.lifespan }}</p>
-      </article>
-      <article>
-        <h3>persönliche Merkmale</h3>
-        <p>Charakter: {{ breed.character }}</p>
-        <p>Rolle: {{ breed.role }}</p>
-      </article>
-      <article>
-        <h3>andere Merkmale</h3>
-        <p>Ursprung: {{ breed.origin_country }}</p>
-        <p>Eingruppierung: {{ breed.section }}</p>
-        <p>FCI-Gruppe: {{ breed.fci_group }}</p>
-      </article>
+      <h2 class="breed-header">Rasseportrait</h2>
+      <img class="breed-img" :src="breed.img" alt="" />
+      <h3>Allgemeine Informationen</h3>
+      <table>
+        <tbody>
+          <tr>
+            <th>Rasse</th>
+            <td>{{ breed.name }}</td>
+          </tr>
+          <tr>
+            <th>Größenzuordnung</th>
+            <td>{{ breed.category }}</td>
+          </tr>
+          <tr>
+            <th>Gewicht</th>
+            <td>{{ breed.weight }}</td>
+          </tr>
+          <tr>
+            <th>Größe</th>
+            <td>{{ breed.size }}</td>
+          </tr>
+          <tr>
+            <th>Farben</th>
+            <td>{{ breed.color }}</td>
+          </tr>
+          <tr>
+            <th>Lebenserwartung</th>
+            <td>{{ breed.lifespan }}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h3>Persönliche Merkmale</h3>
+      <table>
+        <tbody>
+          <tr>
+            <th>Charakter</th>
+            <td>{{ breed.character }}</td>
+          </tr>
+          <tr>
+            <th>Rolle</th>
+            <td>{{ breed.role }}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h3>Andere Merkmale</h3>
+      <table>
+        <tbody>
+          <tr>
+            <th>Ursprung</th>
+            <td>{{ breed.origin_country }}</td>
+          </tr>
+          <tr>
+            <th>Eingruppierung</th>
+            <td>{{ breed.section }}</td>
+          </tr>
+          <tr>
+            <th>FCI-Gruppe</th>
+            <td>{{ breed.fci_group }}</td>
+          </tr>
+        </tbody>
+      </table>
     </section>
     <NuxtLink :to="`/shelters/${shelter.id}`" style="text-decoration: none">
       <div class="shelter-container">
@@ -149,7 +190,67 @@ h1 {
   font-size: 1.2rem;
   cursor: pointer;
 }
+.breed-container {
+  border-radius: 15px;
+  border-color: $d-green;
+  padding: 0.5rem 1.5rem;
+  display: grid;
+  border: 2px solid grey;
+  margin-bottom: 1rem;
+}
+.breed-img {
+  margin-bottom: 1rem;
+  justify-self: center;
+  height: 250px;
+  object-fit: cover;
+  border: transparent;
+  clip-path: circle();
+}
+.breed-header {
+  justify-self: center;
+  font-size: 2rem;
+}
+table {
+  width: 100%;
+  border-collapse: collapse; /* Entfernt doppelte Ränder zwischen Zellen */
+  margin-bottom: 20px;
+  font-family: Poppins, sans-serif;
+}
+
+th {
+  text-align: left;
+  background-color: #f2f2f2;
+  padding: 8px;
+  width: 170px;
+}
+
+td {
+  text-align: left;
+  padding: 8px;
+}
+
+/* Styling für Tabellenzeilen */
+tr:nth-child(even) {
+  background-color: #f9f9f9; /* Hintergrundfarbe für jede zweite Zeile */
+}
+
+tr:hover {
+  background-color: #ddd;
+}
+.shelter-container {
+  display: grid;
+  justify-content: center;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 2rem;
+  gap: 0.2rem;
+}
+.shelter-container h3 {
+  font-size: 2rem;
+}
+
 .shelter-logo {
-  height: 150px;
+  width: 150px;
+  justify-self: center;
 }
 </style>
